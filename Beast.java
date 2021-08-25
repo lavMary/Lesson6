a37273f1042198bc5e5aed01b726f3f5af2c6bfb
lavMary <49761493+lavMary@users.noreply.github.com>
2021-08-25 12:50:05 +0300

    Update Beast.java

--- Beast.java
+++ Beast.java
@@ -1,14 +1,38 @@
 public class Beast {
-    int hp;
-    int damage;
-    String name;
+    private int hp;
+    private int damage;
+    private String name;
 
     public Beast(int hp, int damage, String name) {
         this.hp = hp;
         this.damage = damage;
         this.name = name;
     }
+
+    public int getDamage() {
+        return damage;
+    }
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     final void beastAttack() {
+
         System.out.println("I attack like a beast.");
     }
 }

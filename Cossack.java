072071a3c3950ebb52698cede3d0c0d15b529eaf
lavMary <49761493+lavMary@users.noreply.github.com>
2021-08-25 12:51:13 +0300

    Update Cossack.java

--- Cossack.java
+++ Cossack.java
@@ -1,7 +1,15 @@
 public class Cossack extends Warrior {
-    Saber saber;
+    private Saber saber;
     public Cossack(int hp, int level, int damage) {
         super(hp, level, damage);
+    }
+
+    public Saber getSaber() {
+        return saber;
+    }
+
+    public void setSaber(Saber saber) {
+        this.saber = saber;
     }
 
     @Override

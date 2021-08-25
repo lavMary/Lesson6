59ac3d0737838b49d07dea55ecc90cdafd4743aa
lavMary <49761493+lavMary@users.noreply.github.com>
2021-08-25 12:50:34 +0300

    Update HealerDruid.java

--- HealerDruid.java
+++ HealerDruid.java
@@ -1,9 +1,16 @@
 public class HealerDruid extends Healer{
-    Beast beast;
+    private Beast beast;
     public HealerDruid (int hp, int level, int damage) {
         super(hp, level, damage);
     }
 
+    public Beast getBeast() {
+        return beast;
+    }
+
+    public void setBeast(Beast beast) {
+        this.beast = beast;
+    }
 
     void tameBeast() {
         System.out.println("The beast is tamed.");
